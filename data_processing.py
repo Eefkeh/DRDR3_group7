97a55dd45d2965d99ab71f0d2e13c1769d90e1e7
stefaniekip <151625789+stefaniekip@users.noreply.github.com>
2024-12-13 12:02:28 +0100

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,10 @@ from rdkit import Chem
 from rdkit.Chem import Descriptors 
 from rdkit.Chem.Descriptors import CalcMolDescriptors
 from rdkit.ML.Descriptors import MoleculeDescriptors
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import normalize
+import pandas as pd
+
 
 rdkit.__version__
 
@@ -25,10 +29,20 @@ def calculate_descriptors(data):
     desc_list = [n[0] for n in Descriptors._descList]
     calc = MoleculeDescriptors.MolecularDescriptorCalculator(desc_list)
     outputcsv = 'data.csv'
+    scaler = MinMaxScaler()
     for row in data:
+        descriptors = []
         mol = Chem.MolFromSmiles(row)
-        dict[row]= [calc.CalcDescriptors(mol)]
-    
+        descriptors.append(calc.CalcDescriptors(mol))
+        #normalized_data = scaler.fit_transform(descriptors)
+        normalized_data = normalize(descriptors, norm = 'l2')
+        dict[row]= normalized_data
+    pd.DataFrame.from_dict(dict)
+    #with open("data.csv", "w", newline="") as f:
+       # w = csv.DictWriter(f, dict.keys())
+        #w.writeheader()
+        #w.writerow(dict)
+
     return dict
 
 print(calculate_descriptors('/Users/stefaniekip/Documents/BMT jaar 4/Q2 - Advanced programming/Groeps opdracht/drd-3-binder-quest (1)/train.csv'))
