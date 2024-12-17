987603f44b4bdcad53cfa2f8e27f1fbc2dd0c0d5
Nora Moscardó Weber <n.m.moscardo.weber@student.tue.nl>
2024-12-17 19:14:55 +0100

    Add files via upload

---
+++ t-SNE dimensionality red.py
@@ -0,0 +1,32 @@
+
+#libraries for visualiation
+import matplotlib.pyplot as plt
+import pandas as pd
+from data_processing import file_preparation,get_descriptors #loads the SMILES data + computes molecular descriptors
+from sklearn.manifold import TSNE
+
+def tsne(descriptors):
+    tsne_model=TSNE(n_components=2,perplexity=30,init='pca',random_state=None)
+    tsne_results=tsne_model.fit_transform(descriptors)
+
+    #visualization
+    plt.figure(figsize=(10,7))
+    plt.scatter(tsne_results[:,0],tsne_results[:,1])
+    plt.title('t-SNE visualization with PCA initialization')
+    plt.xlabel('t-SNE dimension 1')
+    plt.ylabel('t-SNE dimension 2')
+    plt.show()
+
+    return tsne_results 
+
+
+if __name__=='__main__':
+    file='dataset.csv'
+
+    #loading smiles data
+    print('loading smiles data')
+    smiles_data=file_preparation(file)
+    descriptor_data,failed_smiles=get_descriptors(smiles_data)
+    tsne_results= tsne(descriptor_data)
+
+    
